da5e0d509def0b46c01dcbad89ab4b846ffe5a8e
rt <r@t.com>
2022-02-07 11:11:27 +0000

    test artifact and commit changes

--- e2e/tests/screenshot.spec.mjs
+++ e2e/tests/screenshot.spec.mjs
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  const buffer = await page.screenshot({ path: 'screenshot.png', fullPage: true });
-  console.log(buffer.toString('base64'));
+  await page.screenshot({ path: '../public/screenshot.png', fullPage: true });
+
 });
